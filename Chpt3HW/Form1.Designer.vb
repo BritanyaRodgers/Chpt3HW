ff45fb6e5f30872e5a192beff4918e4f50b67f19
Chane1204 <89409230+Chane1204@users.noreply.github.com>
2024-09-10 22:15:37 -0400

    Commit 2

--- Chpt3HW/Form1.Designer.vb
+++ Chpt3HW/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,14 +20,14 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.Label1 = New System.Windows.Forms.Label()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
+        Me.btnViewDetails = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -36,9 +36,10 @@ Partial Class Form1
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Myanmar Text", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.ImageAlign = System.Drawing.ContentAlignment.TopCenter
-        Me.Label1.Location = New System.Drawing.Point(255, 9)
+        Me.Label1.Location = New System.Drawing.Point(191, 7)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(206, 50)
+        Me.Label1.Size = New System.Drawing.Size(160, 39)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Fall Marathon"
         Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopCenter
@@ -46,9 +47,10 @@ Partial Class Form1
         'TextBox1
         '
         Me.TextBox1.Font = New System.Drawing.Font("Ink Free", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox1.Location = New System.Drawing.Point(167, 406)
+        Me.TextBox1.Location = New System.Drawing.Point(106, 329)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(2)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(448, 32)
+        Me.TextBox1.Size = New System.Drawing.Size(401, 27)
         Me.TextBox1.TabIndex = 1
         Me.TextBox1.Text = "Full and half-marathon runners are welcome."
         Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
@@ -56,44 +58,48 @@ Partial Class Form1
         'PictureBox1
         '
         Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
-        Me.PictureBox1.Location = New System.Drawing.Point(135, 62)
+        Me.PictureBox1.Location = New System.Drawing.Point(101, 50)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(2)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(542, 327)
+        Me.PictureBox1.Size = New System.Drawing.Size(406, 266)
         Me.PictureBox1.TabIndex = 2
         Me.PictureBox1.TabStop = False
         '
-        'Button1
+        'btnViewDetails
         '
-        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
-        Me.Button1.ForeColor = System.Drawing.Color.HotPink
-        Me.Button1.Location = New System.Drawing.Point(5, 379)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(105, 59)
-        Me.Button1.TabIndex = 3
-        Me.Button1.Text = "View Details"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btnViewDetails.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnViewDetails.ForeColor = System.Drawing.Color.HotPink
+        Me.btnViewDetails.Location = New System.Drawing.Point(4, 308)
+        Me.btnViewDetails.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnViewDetails.Name = "btnViewDetails"
+        Me.btnViewDetails.Size = New System.Drawing.Size(79, 48)
+        Me.btnViewDetails.TabIndex = 3
+        Me.btnViewDetails.Text = "View Details"
+        Me.btnViewDetails.UseVisualStyleBackColor = True
         '
-        'Button2
+        'btnExit
         '
-        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button2.ForeColor = System.Drawing.Color.Crimson
-        Me.Button2.Location = New System.Drawing.Point(706, 398)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 35)
-        Me.Button2.TabIndex = 4
-        Me.Button2.Text = "Exit"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.ForeColor = System.Drawing.Color.Crimson
+        Me.btnExit.Location = New System.Drawing.Point(530, 323)
+        Me.btnExit.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(56, 28)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
+        Me.ClientSize = New System.Drawing.Size(600, 366)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnViewDetails)
         Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Label1)
+        Me.Margin = New System.Windows.Forms.Padding(2)
         Me.Name = "Form1"
         Me.Text = "FallMarathon"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
@@ -105,16 +111,36 @@ Partial Class Form1
     Friend WithEvents Label1 As Label
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents PictureBox1 As PictureBox
-    Friend WithEvents Button1 As Button
+    Friend WithEvents btnViewDetails As Button
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnViewDetails.Click
+
+        MessageBox.Show("Marathon Details: 
+May 30, 2017
+Ryan Park
+Start Time: 8:00 AM
+For more information, view the Fall Marathon Facebook page.")
+
+
+        btnExit.Enabled = True
+
+
+        btnViewDetails.Enabled = False
 
 
     End Sub
 
-    Friend WithEvents Button2 As Button
+    Friend WithEvents btnExit As Button
 
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Close()
+
+
+
+
+    End Sub
+
+    Private Sub frmFallMarathon_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
 End Class
